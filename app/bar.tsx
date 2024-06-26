9c271c670e04b5b1129179629774e849b7b6c792
bo <bohuang1994@gmail.com>
2024-06-26 11:11:42 +0800

    refactor: bar postion code

--- app/bar.tsx
+++ app/bar.tsx
@@ -10,6 +10,7 @@ interface BarProps {
 
 export default function Bar(props: BarProps) {
     let rate_hundred = props.rate * 100
+
     return (
         <div className="w-full bg-slate-800 h-52 flex items-center justify-start gap-16 text-white">
             <div className="w-[70%] h-full">
@@ -18,33 +19,32 @@ export default function Bar(props: BarProps) {
                 </div>
                 <div className="size-full flex justify-start items-center relative">
                     <div className="w-full bg-slate-700 h-3 rounded-full relative border-2 border-gray-500">
-                    </div>
-                    <div className="w-full h-5 absolute flex items-center justify-start">
-                        <div className={cn(
-                            props.colorClass,
-                            `w-[${rate_hundred}%]`,
-                            "h-3 rounded-full")}
-                             style={{width: `${rate_hundred}%`}}></div>
-                        <div className="size-5 bg-slate-700 -ml-3 rounded-full border-[3px] border-white relative">
-                            <div className="absolute -top-12 left-1 flex flex-col items-start">
-                                <div>{rate_hundred}</div>
-                                <div>|</div>
+                        <div className="w-full h-5 absolute top-1/2 -translate-y-1/2 flex items-center justify-start">
+                            <div className={cn(
+                                props.colorClass,
+                                "h-3 rounded-full")}
+                                 style={{width: `${rate_hundred}%`}}></div>
+                            <div className="size-5 bg-slate-700 -ml-3 rounded-full border-[3px] border-white relative">
+                                <div className="absolute -top-12 left-1 flex flex-col items-start">
+                                    <div>{rate_hundred}</div>
+                                    <div>|</div>
+                                </div>
                             </div>
                         </div>
 
-                    </div>
-                    <div
-                        className="size-5 absolute left-[50%] bg-slate-700 -ml-3 rounded-full border-[3px] border-white">
-                        <div className="absolute top-[14px] left-1 flex flex-col items-start w-24">
-                            <div>|</div>
-                            <div>Top 50%: 24</div>
+                        <div
+                            className="size-5 absolute left-[50%] top-1/2 -translate-y-1/2 bg-slate-700 -ml-3 rounded-full border-[3px] border-white">
+                            <div className="absolute top-[14px] left-1 flex flex-col items-start w-24">
+                                <div>|</div>
+                                <div>Top 50%: 24</div>
+                            </div>
                         </div>
-                    </div>
-                    <div
-                        className="size-5 absolute left-[90%] bg-slate-700 -ml-3 rounded-full border-[3px] border-white">
-                        <div className="absolute top-[14px] left-1 flex flex-col items-start w-24">
-                            <div>|</div>
-                            <div>Top 90%: 24</div>
+                        <div
+                            className="size-5 absolute left-[90%] top-1/2 -translate-y-1/2 bg-slate-700 -ml-3 rounded-full border-[3px] border-white">
+                            <div className="absolute top-[14px] left-1 flex flex-col items-start w-24">
+                                <div>|</div>
+                                <div>Top 90%: 24</div>
+                            </div>
                         </div>
                     </div>
                 </div>
