6ea713be7fa874d5720413a403a918c500942a6e
bo <bohuang1994@gmail.com>
2024-06-28 18:24:26 +0800

    feat: add more logic

--- app/bar.tsx
+++ app/bar.tsx
@@ -1,15 +1,19 @@
 import {cn} from "@/lib/utils";
 
 interface BarProps {
-    rate: number,
+    percent: number,
     colorClass: string,
+    borderColorClass: string,
     title: string,
     rightTitle: string,
     rightContent: string,
+    points?: {
+        percent: number,
+        num: number,
+    }[]
 }
 
 export default function Bar(props: BarProps) {
-    let rate_hundred = props.rate * 100
 
     return (
         <div className="w-full bg-slate-800 h-52 flex items-center justify-start gap-16 text-white">
@@ -23,29 +27,30 @@ export default function Bar(props: BarProps) {
                             <div className={cn(
                                 props.colorClass,
                                 "h-3 rounded-full")}
-                                 style={{width: `${rate_hundred}%`}}></div>
+                                 style={{width: `${props.percent}%`}}></div>
                             <div className="size-5 bg-slate-700 -ml-3 rounded-full border-[3px] border-white relative">
                                 <div className="absolute -top-12 left-1 flex flex-col items-start">
-                                    <div>{rate_hundred}</div>
+                                    <div>{props.percent}</div>
                                     <div>|</div>
                                 </div>
                             </div>
                         </div>
 
-                        <div
-                            className="size-5 absolute left-[50%] top-1/2 -translate-y-1/2 bg-slate-700 -ml-3 rounded-full border-[3px] border-white">
-                            <div className="absolute top-[14px] left-1 flex flex-col items-start w-24">
-                                <div>|</div>
-                                <div>Top 50%: 24</div>
+                        {props.points?.map((point, index) => (
+                            <div
+                                key={index}
+                                className={cn(
+                                    point.percent > props.percent ? props.borderColorClass : "border-white",
+                                    "size-5 absolute top-1/2 bg-slate-700 -translate-y-1/2 -ml-3 rounded-full border-[3px]"
+                                )}
+                                style={{left: `${point.percent}%`}}>
+                                <div className="absolute top-[14px] left-1 flex flex-col items-start w-24">
+                                    <div>|</div>
+                                    <div>Top {(100 - point.percent)}%: {point.num}</div>
+                                </div>
                             </div>
-                        </div>
-                        <div
-                            className="size-5 absolute left-[90%] top-1/2 -translate-y-1/2 bg-slate-700 -ml-3 rounded-full border-[3px] border-white">
-                            <div className="absolute top-[14px] left-1 flex flex-col items-start w-24">
-                                <div>|</div>
-                                <div>Top 90%: 24</div>
-                            </div>
-                        </div>
+                        ))
+                        }
                     </div>
                 </div>
             </div>
--- app/page.tsx
+++ app/page.tsx
@@ -5,27 +5,33 @@ export default function Home() {
         <main className="flex min-h-screen flex-col items-center justify-start p-24">
             <div className="size-full p-8 bg-slate-800">
                 <Bar
-                    rate={0.39}
+                    percent={39}
                     colorClass="bg-blue-500"
+                    borderColorClass="border-blue-500"
                     title="This Week's Build Effect"
                     rightTitle="Your Effort index for this week"
                     rightContent="Contribute high-quality code to increase your Effort index."
+                    points={[{percent: 70, num: 34}, {percent: 90, num: 24}]}
                 />
                 <hr className="border-t border-gray-500 my-4"/>
                 <Bar
-                    rate={0.83}
+                    percent={83}
                     colorClass="bg-blue-500"
+                    borderColorClass="border-blue-500"
                     title="This Week's Build Quality"
                     rightTitle="Your Quality index for this week"
                     rightContent="Improve your code quality to increase your Quality index."
+                    points={[{percent: 70, num: 34}, {percent: 90, num: 24}]}
                 />
                 <hr className="border-t border-gray-500 my-4"/>
                 <Bar
-                    rate={0.60}
+                    percent={60}
                     colorClass="bg-yellow-500"
+                    borderColorClass="border-yellow-500"
                     title="This Week's Build Impact"
                     rightTitle="Your Impact index for this week"
                     rightContent="Increase your code's impact to increase your Impact index."
+                    points={[{percent: 50, num: 24}, {percent: 90, num: 24}]}
                 />
             </div>
         </main>
